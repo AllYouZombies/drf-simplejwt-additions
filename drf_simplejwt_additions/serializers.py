778d4bad29a2967efc5beac46a068623242df880
Рустам Астафеев <astafeev0308@gmail.com>
2023-06-14 12:34:33 +0500

    exclude last_login from token

--- drf_simplejwt_additions/serializers.py
+++ drf_simplejwt_additions/serializers.py
@@ -10,7 +10,7 @@ class TokenObtainPairWithFullUserSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
         user_data = {}
-        fields = (i.name for i in User._meta.fields)
+        fields = (i.name for i in User._meta.fields if i.name != 'last_login')
         for field in fields:
             user_data[field] = str(getattr(user, field))
         token['user'] = user_data
